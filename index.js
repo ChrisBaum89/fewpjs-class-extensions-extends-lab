1b2a43851c212043ea5e79cfcaa7a1f409adc86b
Chris Baum <Christopher.Baum@valpo.edu>
2022-03-23 22:22:33 -0400

    commit tree 08445abaf1b757096aa49287b0351dfebb7dcc5b

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Your code here
+class Polygon {
+  constructor(sidesArray) {
+    this.sidesArray = sidesArray
+  }
+
+  get countSides() {
+    return this.sidesArray.length
+  }
+
+  get perimeter(){
+    return this.sidesArray.reduce(
+  (previousValue, currentValue) => previousValue + currentValue)
+  }
+}
+
+class Triangle extends Polygon {
+
+  get isValid() {
+    if (this.countSides == 3) {
+      let validArray = []
+      for (let i = 0; i < 3; i++){
+        let newArray = this.sidesArray.map(x => x)
+        newArray[i] = 0
+        let twoSides = newArray.reduce((previousValue, currentValue) => previousValue + currentValue)
+        validArray[i] = (twoSides > this.sidesArray[i]) ? true:false
+      }
+      return (validArray.includes(false)) ? false : true
+    }
+  }
+}
+
+class Square extends Polygon {
+   get isValid() {
+     let validFormula = x => x == this.sidesArray[0]
+     return this.sidesArray.every(validFormula)
+   }
+
+   get area() {
+     let sideLength = this.sidesArray[0]
+     return (this.isValid) ? (sideLength*sideLength) : 0
+   }
+
+}
